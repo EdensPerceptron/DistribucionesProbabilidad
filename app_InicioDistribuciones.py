00d66a59c23e6cefa3217f6a351b481747755070
Pedro Luis Alcazar Cruz <121834311+EdensPerceptron@users.noreply.github.com>
2023-09-28 10:53:10 -0400

    Add files via upload

---
+++ app_InicioDistribuciones.py
@@ -0,0 +1,28 @@
+# InicioDistribuciones
+import streamlit as st
+
+st.title("Distribuciones de Probabilidad")
+
+st.markdown(
+        """
+        👋 Hola!!
+        Esta aplicación está diseñada para entender las distribuciones de probabilidad.
+        
+        Una distribución de probabilidad basicamente muestra los distintos valores que 
+        puede tomar una variable aleatoria y la frecuencia o probabilidad con la que estos
+        valores se repiten. Existen dos categorías de distribuciones, discretas y continuas.
+    
+        ### Distribuciones Discretas:
+        - [Distribución Uniforme](https://streamlit.io)
+        - Distribución Binomial
+        - Distribución de Poisson [documentation](https://docs.streamlit.io)
+
+        ### Distribuciones continuas
+        - Distribución Normal
+        - Distribución t-Student
+        - Distribución chi-Cuadrado
+        - Distribución exponencial
+        - Distribución Logística
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )

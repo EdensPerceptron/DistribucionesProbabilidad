2c4a163f026b78e5621399e7e3d8102f8187e1b0
Pedro Luis Alcazar Cruz <121834311+EdensPerceptron@users.noreply.github.com>
2023-09-28 16:55:06 -0400

    Update DistNormal.py

--- DistNormal.py
+++ DistNormal.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 
 # Título de la aplicación
 st.title('Distribución Normal')
+st.write('La distribución normal tiene 2 parámetros Media y Desviacion_estándar')
 
 # Sidebar para ajustar los parámetros
-st.sidebar.header('Parámetros de la Distribución Normal')
-media = st.sidebar.number_input('Media', value=0.0)
-desviacion_estandar = st.sidebar.number_input('Desviación Estándar', value=1.0)
-tamaño_muestra = st.sidebar.number_input('Tamaño de la Muestra', value=1000)
+tamaño_muestra = st.number_input('tamaño_muestra', value=1000)
+desviacion_estandar = st.slider('desviacion_estandar', 0.0, 30.0, 10.0)
+media = st.slider('Media', 0.0, 30.0, 20.0)
 
 # Genera una muestra de números aleatorios con distribución normal
 simulacion = np.random.normal(media, desviacion_estandar, tamaño_muestra)
 
 # Crea un histograma para visualizar la distribución simulada
-st.pyplot(plt.hist(simulacion, bins=30, density=True, alpha=0.6, color='b'))
+plt.hist(simulacion, bins=30, density=True, alpha=0.6, color='g',zorder=1)
 
-# Agrega una línea para mostrar la función de densidad de probabilidad teórica
-x = np.linspace(-4, 4, 100)
-pdf = (1 / (desviacion_estandar * np.sqrt(2 * np.pi))) * np.exp(-(x - media)**2 / (2 * desviacion_estandar**2))
-plt.plot(x, pdf, 'k--', linewidth=2)
+    # Agrega una línea para mostrar la función de densidad de probabilidad teórica
+    #x = np.linspace(-4, 4, 100)
+    #pdf = (1 / (desviacion_estandar * np.sqrt(2 * np.pi))) * np.exp(-(x - media)**2 / (2 * desviacion_estandar**2))
+    #plt.plot(x, pdf, 'k--', linewidth=2, zorder=2)
 
 # Etiquetas y título
 plt.xlabel('Valores')
@@ -28,7 +28,6 @@ plt.ylabel('Densidad de Probabilidad')
 plt.title('Simulación de Distribución Normal')
 
 # Muestra la gráfica en Streamlit
-st.pyplot()
+st.pyplot(plt)
 
-# Información adicional
-st.write('Esta aplicación simula una distribución normal con los parámetros especificados en la barra lateral.')
+
